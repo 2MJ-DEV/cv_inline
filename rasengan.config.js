e342aec20ff22cc88770f02101b8be21c0be8954
Jules MUKADI <146131402+2MJ-DEV@users.noreply.github.com>
2025-09-05 15:10:13 +0200

    Ajout de l'adaptateurs vercel

--- rasengan.config.js
+++ rasengan.config.js
@@ -7,7 +7,7 @@ import { configure } from "@rasenganjs/vercel";
 export default defineConfig(async () => {
   return {
     vite: {
-      plugins: [tailwindcss(), rasengan(), rasengan({
+      plugins: [tailwindcss(), rasengan({
 				adapter: configure(),
 			}),],
     },
